d56efbb2fcd1f29c80b6d33649c3be57a787d749
manjaka13 <manjaka.rajaonson@gmail.com>
2023-09-26 14:15:37 +0300

    add footer

---
+++ src/components/CalcFooter.vue
@@ -0,0 +1,23 @@
+<template>
+	<footer class="calc-footer">
+		<p class="calc-footer__left">
+			Made by
+			<a class="link" href="https://harij.vercel.app" title="The developer"
+				>Hari</a
+			>
+		</p>
+		<p class="calc-footer__right">Powered by VueJS</p>
+	</footer>
+</template>
+
+<script>
+import "@/styles/calc-footer.scss";
+
+/**
+ * Calculator footer
+ */
+
+export default {
+	name: "CalcFOoter",
+};
+</script>
